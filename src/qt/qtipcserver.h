d9f83c255fc0cd4da6cb79ae05ec17c719877007
Darkhashmaster <dhashcompany@gmail.com>
2017-08-10 08:35:55 +0100

    Axiontoken Source
    a

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define AxionToken-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "AxionTokenURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
